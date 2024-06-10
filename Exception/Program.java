e80e3d23b31ea6e4559e047071e48090c943b4e3
Pooja Tajane <92642653+poojatajane18@users.noreply.github.com>
2024-06-10 16:18:46 +0530

    Add files via upload

---
+++ Exception/Program.java
@@ -0,0 +1,28 @@
+package com.Exception;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1");
+		try {
+			System.out.println("2");
+			int a=10,b=2,c=0;
+			System.out.println("3");
+			c=a/c;
+			System.out.println("4");
+			System.out.println(c);
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("5");
+			System.out.println("c");
+			System.out.println("6");
+			// TODO: handle exception
+		}
+		System.out.println("Railworld");
+
+	}
+
+}
